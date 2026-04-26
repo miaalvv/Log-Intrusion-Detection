77966ca566d0bd05266ab9cff58817ddaaa8c23a
miaalvv <alvarezmia1227@gmail.com>
2026-04-26 14:31:18 -0700

    Update alert.py

--- src/alert.py
+++ src/alert.py
@@ -1,1 +1,9 @@
+def generate_alerts(suspicious_users):
+    alerts = []
 
+    for user_data in suspicious_users:
+        message = f"[ALERT] User {user_data['user']} has {user_data['failed_attempts']} failed login attempts"
+        print(message)
+        alerts.append(message)
+
+    return alerts
